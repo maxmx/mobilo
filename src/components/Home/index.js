a1cc6a4ed46cca5268a68b28e8443de8b96a3e6d
maxmx <maximeb@gmail.com>
2022-05-26 18:36:37 -0400

    Passes

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,12 +7,14 @@ import Day from './day';
 import hero2022 from '../../img/hero-2022.jpg';
 import { Poster } from '../Hero/styles';
 import { SiteNote } from '../styles';
+import Promo from './promo';
 
 export default ({ images, dates = [], content }) => (
   <>
     <Poster style={{ paddingTop: '3.25rem' }}>
       <img src={hero2022} alt="Dr. Mobilo Aquafest, 20 au 26 juin 2022" />
     </Poster>
+    <Promo />
 
     <h1
       style={{
@@ -32,6 +34,8 @@ export default ({ images, dates = [], content }) => (
           <Day {...{ key, images }} shows={dates[key]} index={i} />
         ))}
       </Row>
+
+      <Promo />
 
       <HTMLContent {...{ content }} />
       <SiteNote>
